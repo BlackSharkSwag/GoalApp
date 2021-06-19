95df03d9dfe0df36cfc4dc27d797ff26825693fb
juliobras <juliocesarmonteiro2.0@outlook.com>
2021-06-20 09:34:00 +1000

    I added a new button in our MainActivity so it would open our GoalData activity. So basically, when the the Button "Add a New Goal" is hit then our Add Goal acvity is opened.

--- Team09 App/Team09App/app/src/main/java/com/example/team09app/MainActivity.java
+++ Team09 App/Team09App/app/src/main/java/com/example/team09app/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.team09app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View v) {
+                                          openActivity2();
+
+
+
+                                      }
+        });
+
 
         /*DataConfig data = new DataConfig();
         data.saveData(this);*/
@@ -18,5 +32,11 @@ public class MainActivity extends AppCompatActivity {
 /*        for(GoalData goal:data.goals.data) { //load and access data
             System.out.println(goal.getDate());
         }*/
+    }
+    public void openActivity2(){
+        Intent intent;
+        intent = new Intent(this, AddGoals.class);
+        startActivity(intent);
+
     }
 }

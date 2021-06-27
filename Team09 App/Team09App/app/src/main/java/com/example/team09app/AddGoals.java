07dc1d70640d4732c866cfec609954f8b9ef38d1
juliobras <juliocesarmonteiro2.0@outlook.com>
2021-06-27 11:27:28 +1000

    I have added a Toast for when the Goal is created successfully On the AddGoals I converted our button to a lambda. I also created an instance of recylerView in our main so we can display our adaptor to the recyclerview but I got stuck on it.

--- Team09 App/Team09App/app/src/main/java/com/example/team09app/AddGoals.java
+++ Team09 App/Team09App/app/src/main/java/com/example/team09app/AddGoals.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddGoals extends AppCompatActivity {
 
@@ -16,24 +17,22 @@ public class AddGoals extends AppCompatActivity {
         setContentView(R.layout.activity_add_goals);
 
         Button button = (Button) findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText editText = (EditText) findViewById(R.id.goalField);
-                String text = editText.getText().toString();
+        button.setOnClickListener(v -> {
+            EditText editText = (EditText) findViewById(R.id.goalField);
+            String text = editText.getText().toString();
 
-                EditText editText2 = (EditText) findViewById(R.id.descriptionField);
-                String text2 = editText2.getText().toString();
+            EditText editText2 = (EditText) findViewById(R.id.descriptionField);
+            String text2 = editText2.getText().toString();
 
-                CalendarView editText3 = (CalendarView) findViewById(R.id.calendarView);
-                Long text3 = editText3.getDate();
-                String dateString = text3.toString();
+            CalendarView editText3 = (CalendarView) findViewById(R.id.calendarView);
+            Long text3 = editText3.getDate();
+            String dateString = text3.toString();
 
-                GoalData object = new GoalData(text, text2, dateString);
-                DataConfig.getInstance().goals.data.add(object);
-                DataConfig.getInstance().saveData(AddGoals.this);
-                finish();
-            }
+            GoalData object = new GoalData(text, text2, dateString);
+            DataConfig.getInstance().goals.data.add(object);
+            DataConfig.getInstance().saveData(AddGoals.this);
+            finish();
+            Toast.makeText(AddGoals.this,"Goal saved successfully",Toast.LENGTH_LONG).show();
         });
 
     }
